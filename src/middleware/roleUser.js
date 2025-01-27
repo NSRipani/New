72b78ca9eb2029272cd35fe6cb23c8b953c3622a
NSRipani <nicor.33@live.com>
2025-01-27 19:30:57 -0300

    fix: Entraga-final

--- src/middleware/roleUser.js
+++ src/middleware/roleUser.js
@@ -1,9 +1,9 @@
 export const roleAuth = (role) => {
     return async(req, res, next)=>{
-        // if(!req.user) return res.status(401).json({ message: 'You are not authorized for this section', error: 'Unauthorized' });
-        if(!req.user) return res.status(401).json({ error : 'You are not authorized for this section'}); // , error: 'Unauthorized' 
+        console.log("User:", req.user)
+        if(!req.user) return res.status(401).json({ error : 'You are not authorized for this section'});
 
-        if(req.user.role !== role) return res.status(403).json({ error: 'only administrators handle products' })
+        if(req.user.role !== role) return res.status(403).json({ error: "You don't have permissions, only an administrator can execute this operation" })
 
         next()
     }
--- src/route/apis/api.products.js
+++ src/route/apis/api.products.js
@@ -1,15 +1,19 @@
 import { Router } from "express";
 import productController from './../../controllers/product.controller.js';
 import { isValidatorProduct } from './../../middleware/isValidatorProduct.js';
+import { roleAuth } from './../../middleware/roleUser.js';
+import { passportCall } from "../../passport/passportCall.js";
 
 const prodRouter = Router()
 
-prodRouter.post("/", isValidatorProduct, productController.create)
+prodRouter.use(passportCall("current"))
+
+prodRouter.post("/", roleAuth('admin'), isValidatorProduct, productController.create)
 prodRouter.get("/", productController.readAll)
 prodRouter.get("/filter", productController.readCategory)
 prodRouter.get("/paginate", productController.paginate)
-prodRouter.get("/:id", productController.readIdProduct)
-prodRouter.put("/:id", isValidatorProduct, productController.update)
-prodRouter.delete("/:id", productController.delete)
+prodRouter.get("/:id", roleAuth('admin'), productController.readIdProduct)
+prodRouter.put("/:id", roleAuth('admin'), isValidatorProduct, productController.update)
+prodRouter.delete("/:id", roleAuth('admin'), productController.delete)
 
 export default prodRouter;
